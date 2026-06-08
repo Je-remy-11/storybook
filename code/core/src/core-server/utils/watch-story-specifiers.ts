9d28cfd5a9c551265d2d8efe13228b0a7c3dc693
hyk <1770848824@qq.com>
2026-06-08 07:07:13 +0000

    1

--- code/core/src/core-server/utils/watch-story-specifiers.ts
+++ code/core/src/core-server/utils/watch-story-specifiers.ts
@@ -55,7 +55,7 @@ export function watchStorySpecifiers(
   const wp = new Watchpack({
     // poll: true, // Slow!!! Enable only in special cases
     followSymlinks: false,
-    ignored: ['**/.git', '**/node_modules'],
+    ignored: ['**/.git', '**/node_modules', '**/.nx/cache/**', '**/tsconfig.json'],
   });
   wp.watch({ files, directories });
 
--- code/core/src/core-server/utils/watchConfig.ts
+++ code/core/src/core-server/utils/watchConfig.ts
@@ -10,7 +10,7 @@ export function watchConfig(
 ) {
   const wp = new Watchpack({
     followSymlinks: false,
-    ignored: ['**/.git', '**/node_modules'],
+    ignored: ['**/.git', '**/node_modules', '**/.nx/cache/**', '**/tsconfig.json'],
   });
 
   wp.watch({
